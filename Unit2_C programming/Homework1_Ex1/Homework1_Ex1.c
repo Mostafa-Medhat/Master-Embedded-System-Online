1b7fca9a930f654877d04190b619fe21ff89ac23
Mostafa-Medhat <mmedhat72@gmail.com>
2024-05-14 15:47:01 +0300

    Homework1_ex1

---
+++ Unit2_C programming/Homework1_Ex1/Homework1_Ex1.c
@@ -0,0 +1,13 @@
+/***************************************
+ * 	Homework1_Ex1.c
+ *  Author: Mostafa Medhat
+ ***************************************/
+#include<stdio.h>
+
+int main(void)
+{
+	printf("C Programming");
+	return 0;
+
+}
+
